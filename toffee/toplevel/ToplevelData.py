3690b72367d09f352618d970e263881fa4321f44
Chan <chandler14362@gmail.com>
2015-05-22 23:54:19 -0400

    toplevel: Differentiate elements between files

--- toffee/toplevel/ToplevelData.py
+++ toffee/toplevel/ToplevelData.py
@@ -1,5 +1,7 @@
 import collections
 
+import itertools
+
 from xml.etree import ElementTree
 
 from toffee.error.Error import ToffeeSyntaxError
@@ -8,16 +10,18 @@ from toffee.element import ElementPool
 
 class ToplevelData(collections.MutableMapping):
     def __init__(self):
-        self.meta = {}
-        self.elements = []
+        self.meta = collections.OrderedDict()
+        self.elements = collections.OrderedDict()
 
     def getToplevelElements(self):
-        return self.elements
+        return tuple(itertools.chain(*self.elements.values()))
 
     def readTof(self, path):
         pass
 
     def readTml(self, path):
+        elements = []
+
         tree = ElementTree.parse(path)
         root = tree.getroot()
 
@@ -28,8 +32,10 @@ class ToplevelData(collections.MutableMapping):
                 raise ToffeeSyntaxError('Invalid child "%s" for root "%s"' %
                                         (child.tag, root.tag))
 
-            self.elements.append(element)
+            elements.append(element)
             element.readTml(self, child)
+
+        self.elements[path] = elements
 
     def get(self, item, default=None):
         return self.meta.get(item, default)
